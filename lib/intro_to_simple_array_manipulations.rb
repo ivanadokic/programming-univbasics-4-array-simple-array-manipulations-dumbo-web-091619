2cecbcb5a982a54b02cbf2aaed256c10d0345ef8
dependable-helper-6910 <ivana_pile@yahoo.com>
2019-09-07 17:56:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_push
+Names = ["Hoze", "Sam" "George", "Lion"]
+next_name = ["Nick"]
+using_push. push(next_name)
+end
+
+def next_names
+Names = ["Hoze", "Sam" "George", "Lion"]
+next_name = ["Nick"]
+Names.unshift(next_name)
+end
+
+def using_pop
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop
+return arists
+end
+
+def pop_with_args
+rtists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop_with_args
+return arists
+
+def using_concat
+X = [1, 2, 3, 4]
+Y= [5, 6, 7]
+x.concat y
+return x+Y
+end
